8eab94212b05b397071b58b09d8e58d3d65010bf
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:20 -0700

    exported from Skill Struck

---
+++ odd/python dictionaries.py
@@ -0,0 +1,15 @@
+author = {
+    "Brother" : 1,
+    "Mother" : 2,
+    "Father" : 3,
+    "Uncle" : 4,
+    "Son in law" : 5, 
+    "law" : 6, 
+    "seven" : 7,
+    "eight" : 8,
+    "nine" : 9,
+    "ten!" : 10
+}
+
+
+print(author) 
