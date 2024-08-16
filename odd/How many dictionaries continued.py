21c9714da6566c74309ff4fb7c2c72bf08af9a22
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:26 -0700

    exported from Skill Struck

---
+++ odd/How many dictionaries continued.py
@@ -0,0 +1,14 @@
+dictionary = { 1: "bicycle", 2: "soccer balls", 3: "piano books" }
+
+
+item1 = input("What is something you have 4 of?")
+item2 = input("What is something you have 5 of?") 
+item3 = input("What is something you have 6 of?") 
+
+
+dictionary[4] = item1 
+dictionary[5] = item2
+dictionary[6] = item3 
+
+
+print(dictionary) 
