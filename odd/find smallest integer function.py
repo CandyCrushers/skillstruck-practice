21f5a1695be2934a5005d51737ee8f1cc1668c60
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:18 -0700

    exported from Skill Struck

---
+++ odd/find smallest integer function.py
@@ -0,0 +1,10 @@
+def smallest(): 
+    num1 = int(input("Enter a number!")) 
+    num2 = int(input("Eneter a number!")) 
+
+    if num1 > num2: 
+        print(num2) 
+    else: 
+        print(num1) 
+
+smallest() 
