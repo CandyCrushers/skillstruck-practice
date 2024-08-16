ba493b8c00420746142e747a3d06abf137376fb6
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:18 -0700

    exported from Skill Struck

---
+++ odd/how many are equal function.py
@@ -0,0 +1,20 @@
+def equals(): 
+    num1 = int(input("Enter us a number!")) 
+    num2 = int(input("Enter us a number!"))
+    num3 = int(input("Enter us a number!"))
+
+    if num1 == num2 == num3:  
+        print(3) 
+    elif num1 == num2: 
+        print(2) 
+    elif num2 == num3:
+        print(2) 
+    elif num1 == num3: 
+        print(2)
+    else: 
+        print(0) 
+   
+
+
+
+equals()  
