7fb5421e729a69aea9a69f7ae0b52ad2217b1ac8
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:23 -0700

    exported from Skill Struck

---
+++ odd/color dictionary .py
@@ -0,0 +1,15 @@
+colors = {
+    "red" : "green",
+    "blue" : "orange",
+    "orange" : "blue", 
+    "purple" : "yellow",
+    "violet" : "purple", 
+} 
+
+
+
+print(colors["orange"]) 
+print(colors["purple"]) 
+colors["red"] = "blue"
+colors["blue"] = "red" 
+print(colors) 
