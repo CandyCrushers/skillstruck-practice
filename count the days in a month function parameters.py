b97e8d92104d6cf5fb28dd3aa053a4d8763f4fce
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:14 -0700

    exported from Skill Struck

---
+++ count the days in a month function parameters.py
@@ -0,0 +1,29 @@
+def months(): 
+    month = int(input("Enter a month!"))
+    if month == 1: 
+        print(31) 
+    elif month == 2: 
+        print(28)
+    elif month == 3: 
+        print(31) 
+    elif month == 4: 
+        print(30) 
+    elif month == 5: 
+        print(31) 
+    elif month == 6: 
+        print(30) 
+    elif month == 7:
+        print(31) 
+    elif month == 8: 
+        print(31) 
+    elif month == 9: 
+        print(30) 
+    elif month == 10: 
+        print(31) 
+    elif month == 11: 
+        print(30) 
+    elif month == 12: 
+        print(31) 
+    
+
+months() 
