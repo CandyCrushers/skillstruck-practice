c7b9f2f3dee88db59de1fbba7c63e0e9f748882e
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:24 -0700

    exported from Skill Struck

---
+++ arborist dictionary .py
@@ -0,0 +1,8 @@
+trees = { "aspen" : 75, "pine" : 82, "maple" : 60, "oak" : 65, "willow" : 80, "cottonwood" : 100 }
+
+removal = input("What tree do you wish removed?") 
+
+
+trees.pop(removal) 
+
+print(trees) 
