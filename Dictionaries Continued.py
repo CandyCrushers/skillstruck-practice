cce634a056be9699cf799fcefb23f95cb8131fd4
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:26 -0700

    exported from Skill Struck

---
+++ Dictionaries Continued.py
@@ -0,0 +1,14 @@
+coins = { 
+    "Pennies" : 1,
+    "Nickes" : 5,
+    "Dimes" : 10,
+    "Quarters" : 25,
+}
+
+coins["silver dollar"] = 100 
+coins.pop("Pennies") 
+
+print(coins) 
+print(len(coins)) 
+
+
