7856fa4fd309012c7376d1c30a43c97c3f1e7913
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:15 -0700

    exported from Skill Struck

---
+++ odd/A party for letter C function parameters .py
@@ -0,0 +1,12 @@
+def snack_Checker():  
+    snack = input("Enter a snack!") 
+    if snack[0].lower() == "c": 
+        print(snack + " can be a part of the party!") 
+    else: 
+        print("Sorry! " + snack + " doesn't start with the letter c!") 
+
+
+
+
+
+snack_Checker() 
