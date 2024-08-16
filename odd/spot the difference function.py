9976ea9bd140a8c037820dcd0c90695b0cb6b3fb
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:19 -0700

    exported from Skill Struck

---
+++ odd/spot the difference function.py
@@ -0,0 +1,16 @@
+def spotdif(): 
+    num1 = int(input("Enter a number!"))
+    num2 = int(input("Enter a number!")) 
+    num3 = int(input("Enter a number!")) 
+
+    if num1 == num2: 
+        print(3)
+    elif num2 == num3: 
+        print(1) 
+    elif num3 == num1: 
+        print(2) 
+    else: 
+        print(0) 
+
+
+spotdif() 
