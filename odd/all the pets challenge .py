b54ee75b4d950e0d644fef0881e95e9540be7bf1
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:23 -0700

    exported from Skill Struck

---
+++ odd/all the pets challenge .py
@@ -0,0 +1,16 @@
+pets = { 
+    "fish" : 30, 
+    "dogs" : 2, 
+    "chickens" : 5, 
+    "cats" : 2, 
+    "bunny" : 1 
+}
+
+
+
+pets["fish"] = 20 
+pets["bunny"] = 7 
+
+print("Because 10 fish died, and the bunny had  6 babies, you now have, " + str(pets["fish"]) + " and " + str(pets["bunny"]) + " at your house.")
+
+print(pets) 
