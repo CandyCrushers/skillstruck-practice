486d47b202673bad6f67b336a942c06842460ea2
D1Programmerr <sendmestuffnowplz@gmail.com>
2024-08-16 14:06:10 -0700

    exported from Skill Struck

---
+++ start the game def challenge.py
@@ -0,0 +1,14 @@
+choose = input("Would you like to start game?") 
+
+def game(): 
+    print("Initialization Complete") 
+
+def game2(): 
+    print("Initialization Failed") 
+
+if choose == "yes": 
+    game() 
+else: 
+    game2() 
+
+
